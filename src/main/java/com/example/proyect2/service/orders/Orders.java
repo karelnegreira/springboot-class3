5a72e3604b3a6dfbe390e1532ce15ee0327bb5e7
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-12-02 21:27:34 +1200

    OrdersController class added

--- src/main/java/com/example/proyect2/service/orders/Orders.java
+++ src/main/java/com/example/proyect2/service/orders/Orders.java
@@ -2,10 +2,30 @@ package com.example.proyect2.service.orders;
 
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
 public class Orders {
+}
+
+@Controller
+@ResponseBody
+class OrdersController {
+
+    private final OrderRepository repository;
+
+    OrdersController(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    void create(@RequestBody Order order) {
+
+    }
 }
 
 
